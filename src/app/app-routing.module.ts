7dd24a15fe077ed4ac1c5b1edd4fb464178edd7d
22ftt1499 <22ftt1499@student.pb.edu.bn>
2024-04-06 12:05:43 +0800

    renaming the pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,25 +4,29 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  {
+    loadChildren: () =>
+      import('./tabs/tabs.module').then((m) => m.TabsPageModule),
+  },
+  {
     path: 'tab4',
-    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
+    loadChildren: () =>
+      import('./ProfilePage/tab4.module').then((m) => m.Tab4PageModule),
   },
   {
     path: 'feedback',
-    loadChildren: () => import('./feedback/feedback.module').then( m => m.FeedbackPageModule)
+    loadChildren: () =>
+      import('./feedback/feedback.module').then((m) => m.FeedbackPageModule),
   },
   {
     path: 'aboutus',
-    loadChildren: () => import('./aboutus/aboutus.module').then( m => m.AboutusPageModule)
-  }
-
+    loadChildren: () =>
+      import('./aboutus/aboutus.module').then((m) => m.AboutusPageModule),
+  },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
