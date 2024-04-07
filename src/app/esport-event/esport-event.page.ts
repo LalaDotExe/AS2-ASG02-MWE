77f1a2dae2e74e636a4a9c765dc9aad1d4cd0229
22ftt1499 <22ftt1499@student.pb.edu.bn>
2024-04-07 20:49:49 +0800

    adding card, image and text description for the esport event page

--- src/app/esport-event/esport-event.page.ts
+++ src/app/esport-event/esport-event.page.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./esport-event.page.scss'],
 })
 export class EsportEventPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
