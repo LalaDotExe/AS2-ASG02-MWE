52c0b62460fed4bba5e1970a62b39b6cc13b51ba
22ftt1499 <22ftt1499@student.pb.edu.bn>
2024-04-07 16:59:38 +0800

    add search filter function

--- src/app/ActivityPage/tab2.page.ts
+++ src/app/ActivityPage/tab2.page.ts
@@ -3,10 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  searchTerm: string = '';
 
-  constructor() {}
+  clubsAndEvents = [
+    { name: 'Esport Convo Event', imageUrl: 'assets/img/PestaConvo.jpg' },
+    { name: 'Esport Club', imageUrl: 'assets/img/EsportClub.jpg' },
+    { name: 'Golden Night Event', imageUrl: 'assets/img/GoldenNight.jpg' },
+    { name: 'Handball Club', imageUrl: 'assets/img/handball.jpg' },
+  ];
 
+  filteredClubsAndEvents = this.clubsAndEvents;
+
+  filterClubsAndEvents() {
+    this.filteredClubsAndEvents = this.clubsAndEvents.filter((item) => {
+      return item.name.toLowerCase().includes(this.searchTerm.toLowerCase());
+    });
+  }
 }
