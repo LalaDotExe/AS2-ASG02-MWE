2a2de33f92ff350e4340a48cd737b3380cccb0c2
22ftt1499 <22ftt1499@student.pb.edu.bn>
2024-04-07 16:53:11 +0800

    importing images and adding card for manage club

--- src/app/manage-club/manage-club.page.ts
+++ src/app/manage-club/manage-club.page.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-club.page.scss'],
 })
 export class ManageClubPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
